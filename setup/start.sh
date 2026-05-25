8f9d51c06405fdcb95661544c0979a39434ed280
boom <boom@boomy.wtf>
2026-05-25 10:22:44 +1000

    Part 1: Remove Nextcloud (CalDAV/CardDAV/files)
    Delete setup/nextcloud.sh, owncloud tools/scripts, and miab-owncloud fail2ban
    filter. Strip Nextcloud nginx blocks from primaryonly.conf and replace with
    oxi.email catch-all + FileBrowser /files/ proxy stubs. Remove owncloud-backup
    duplicity exclusions, CalDAV/CardDAV payloads from ios-profile.xml, and
    miab-owncloud fail2ban jail. Stub out sync-guide.html.

--- setup/start.sh
+++ setup/start.sh
@@ -114,7 +114,7 @@ source setup/dkim.sh
 source setup/spamassassin.sh
 source setup/web.sh
 source setup/webmail.sh
-source setup/nextcloud.sh
+source setup/filebrowser.sh
 source setup/zpush.sh
 source setup/management.sh
 source setup/munin.sh
